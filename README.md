352b029f8d3cdaaef09299afe2edf08c3789146a
Samiran Saha <samiran.s89@gmail.com>
2021-02-08 21:13:08 +0530

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # browserstack_session_stats
 This repository helps analyse the BrowserStack Automate test sessions for any latency issues
+
+## How to use?
+
+- Ensure you have [Ruby](https://www.ruby-lang.org/en/downloads/) (2.x or 3.x) setup on your machine.
+
+- Run analysis for a particular test session
+BROWSERSTACK_USERNAME=<YOUR_BROWSERSTACK_USERNAME> BROWSERSTACK_ACCESS_KEY=<YOUR_BROWSERSTACK_ACCESS_KEY> ruby fetch_bstack_session_stats.rb session <BROWSERSTACK_SESSION_ID>
+
+- Run analysis for an entire test build
+BROWSERSTACK_USERNAME=<YOUR_BROWSERSTACK_USERNAME> BROWSERSTACK_ACCESS_KEY=<YOUR_BROWSERSTACK_ACCESS_KEY> ruby fetch_bstack_session_stats.rb build <BROWSERSTACK_BUILD_ID>
+
+- In case, you would like to tailor the thresholds for inside_time, outside_time, session_stop_time, please update the same in config.yml in seconds.
+
+- Inside time for each Selenium command is defined as the time taken to process the command by BrowserStack. i.e. (current command's response time) minus (current command's request time), both of them as logged in Test session's Raw logs
+
+- Outside time for each Selenium command is defined as the time taken required to receive the next command from the user's infrastructure after BrowserStack has sent the previous command's response. i.e. (current command's request time) minus (previous command's response time), both of them as logged in Test session's Raw logs
---
+++ fetch_bstack_session_stats.rb
@@ -0,0 +1,135 @@
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'rest-client'
+require 'json'
+require 'net/http'
+require 'time'
+require 'yaml'
+
+
+class Command
+  def initialize(curr_timestamp, command_type, full_command)
+    @curr_timestamp = curr_timestamp
+    @command_type = command_type
+    @full_command = full_command
+  end
+
+  def get_command_type
+    @command_type
+  end
+
+  def get_curr_timestamp
+    @curr_timestamp
+  end
+
+  def get_full_command
+    @full_command
+  end
+end
+
+begin
+  session_hash_data_to_analyse = {}
+  analysis_mode = ARGV[0]
+  build_or_session_id = ARGV[1]
+  limit = 100
+  offset = 0
+  session_count_iterator = 1
+  config_array = YAML.load(File.read("config.yml"))
+
+  puts "Session_ID\tInside_Time\tOutside_Time\tUnaccounted_Time\tTotal_Session_Time\tInside_Time_Percent\tOutside_Time_Percent\tUnaccounted_Time_Percentage\tTotal_REQs\tSTOP_SESSION_Time"
+
+  loop do
+    if analysis_mode == 'build'
+      base_url = "https://#{ENV['BROWSERSTACK_USERNAME']}:#{ENV['BROWSERSTACK_ACCESS_KEY']}@api.browserstack.com/automate/builds/#{build_or_session_id}/sessions.json?limit=#{limit}&offset=#{offset}"
+    elsif analysis_mode == 'session'
+      base_url = "https://#{ENV['BROWSERSTACK_USERNAME']}:#{ENV['BROWSERSTACK_ACCESS_KEY']}@api.browserstack.com/automate/sessions/#{build_or_session_id}.json"
+    end
+
+    results = RestClient.get(base_url)
+    results_json = JSON.parse(results.body)
+
+    if (analysis_mode == 'build' && !results_json.empty?) || ((analysis_mode == 'session' && session_count_iterator == 1))
+      session_count_iterator += 1
+      (0..results_json.length - 1).each do |i|
+        if results_json.length > 1
+          automation_session_raw_logs = results_json[i]['automation_session']['logs']
+          automation_session_duration = results_json[i]['automation_session']['duration']
+          session_id = results_json[i]['automation_session']['hashed_id']
+        else
+          automation_session_raw_logs = results_json['automation_session']['logs']
+          automation_session_duration = results_json['automation_session']['duration']
+          session_id = results_json['automation_session']['hashed_id']
+        end
+        latency_data_to_analyse = []
+        uri = URI(automation_session_raw_logs)
+
+        req = Net::HTTP::Get.new(uri)
+        req.basic_auth (ENV['BROWSERSTACK_USERNAME']).to_s, (ENV['BROWSERSTACK_ACCESS_KEY']).to_s
+
+        res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+          http.request(req)
+        end
+        cmd_list = []
+        res.body.lines.each do |line|
+          line_split = line.split
+          next if line_split.empty?
+          ts_split = line_split[1].split(':')
+          cmd = Command.new(Time.parse("#{line_split[0]} #{ts_split[0]}:#{ts_split[1]}:#{ts_split[2]}.#{ts_split[3]}", '%Y-%m-%d %H:%M:%S.%3N'), (line_split[2]).to_s, line.to_s)
+          cmd_list.push(cmd)
+        end
+
+        inside_time = 0.0
+        outside_time = 0.0
+        stop_session_time = 0.0
+        tot_reqs = 0
+        (0..cmd_list.length - 1).each do |i|
+          if !cmd_list[i + 1].nil? && cmd_list[i].get_command_type == 'REQUEST' && cmd_list[i + 1].get_command_type == 'RESPONSE'
+            curr_resp_minus_req = (cmd_list[i + 1].get_curr_timestamp - cmd_list[i].get_curr_timestamp)
+            inside_time += curr_resp_minus_req
+            tot_reqs += 1
+            if curr_resp_minus_req > config_array["config"]["inside_time_threshold"]
+              latency_data_to_analyse.push "HIGH INSIDE TIME: #{curr_resp_minus_req} seconds.\nRESPONSE:#{cmd_list[i + 1].get_curr_timestamp} #{cmd_list[i + 1].get_command_type}\nREQUEST      :#{cmd_list[i].get_curr_timestamp} #{cmd_list[i].get_command_type}\nFULL COMMANDS:\nRESPONSE:#{cmd_list[i + 1].get_full_command}\nREQUEST:#{cmd_list[i].get_full_command}\n\n"
+            end
+          end
+          if !cmd_list[i + 1].nil? && cmd_list[i].get_command_type == 'RESPONSE' && cmd_list[i + 1].get_command_type == 'REQUEST'
+            curr_req_minus_prev_resp = (cmd_list[i + 1].get_curr_timestamp - cmd_list[i].get_curr_timestamp)
+            outside_time += curr_req_minus_prev_resp
+            if curr_req_minus_prev_resp > config_array["config"]["outside_time_threshold"]
+              latency_data_to_analyse.push "HIGH OUTSIDE TIME: #{curr_req_minus_prev_resp} seconds.\nREQUEST      :#{cmd_list[i + 1].get_curr_timestamp} #{cmd_list[i + 1].get_command_type}\nPREV RESPONSE:#{cmd_list[i].get_curr_timestamp} #{cmd_list[i].get_command_type}\nFULL COMMANDS:\nREQUEST:#{cmd_list[i + 1].get_full_command}\nPREV RESPONSE:#{cmd_list[i].get_full_command}\n\n"
+            end
+          end
+          next unless !cmd_list[i + 1].nil? && cmd_list[i].get_command_type == 'RESPONSE' && cmd_list[i + 1].get_command_type == 'STOP_SESSION'
+
+          stop_session_time = (cmd_list[i + 1].get_curr_timestamp - cmd_list[i].get_curr_timestamp)
+          if stop_session_time > config_array["config"]["session_stop_time_threshold"]
+            latency_data_to_analyse.push "HIGH SESSION STOP TIME: #{stop_session_time} seconds.\nSESSION STOP:#{cmd_list[i + 1].get_curr_timestamp} #{cmd_list[i + 1].get_command_type}\nPREV RESPONSE:#{cmd_list[i].get_curr_timestamp} #{cmd_list[i].get_command_type}\nFULL COMMANDS:\nSESSION STOP:#{cmd_list[i + 1].get_full_command}\nPREV RESPONSE:#{cmd_list[i].get_full_command}\n\n"
+          end
+        end
+        session_hash_data_to_analyse[session_id] = latency_data_to_analyse
+
+        delta_unaccounted_time = automation_session_duration - inside_time - outside_time - stop_session_time
+        inside_time_per = inside_time * 100 / automation_session_duration
+        outside_time_per = outside_time * 100 / automation_session_duration
+        delta_unaccounted_time_per = delta_unaccounted_time * 100 / automation_session_duration
+        puts "#{session_id}\t#{inside_time.round(3)}\t#{outside_time.round(3)}\t#{delta_unaccounted_time.round(3)}\t#{automation_session_duration.round(3)}\t#{inside_time_per.round(3)}\t#{outside_time_per.round(3)}\t#{delta_unaccounted_time_per.round(3)}\t#{tot_reqs}\t#{stop_session_time}"
+      end
+    else
+      break
+    end
+    offset += limit
+  end
+
+  puts "\n\n"
+  session_hash_data_to_analyse.each do |key, value|
+    puts "Session id: #{key}\n"
+    value.each do |entry|
+      puts "Latency info entry: \n#{entry}\n"
+    end
+    puts "--------------------------------------------------\n\n"
+  end
+rescue StandardError => e
+  puts "A runtime exception occurred while processing the request: #{e.message}"
+  print "Exception trace:\n"
+  puts e.backtrace
+end
